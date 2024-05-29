308c8e232c5b123a84ae15af6b7028f74201b098
Kirill Karavaev <102624537+h3ct0rzzz@users.noreply.github.com>
2024-05-29 20:40:18 +0300

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,6 +12,14 @@ from src.features.build_features import (
 KREMLIN_COORDS = (55.7520, 37.6175)
 AVERAGE_WALK_SPEED = 5
 
+FEATURE2DROP = [
+    "index_right",
+    "lat",
+    "osm_transport_stop_points_in_0.01",
+    "lng",
+    "osm_crossing_points_in_0.01",
+]
+
 
 def json_to_dataframe(json_data: dict) -> pd.DataFrame:
     data: dict = json_data
@@ -29,5 +37,7 @@ def add_features(df: pd.DataFrame, geo, stations) -> pd.DataFrame:
                 add_kremlin_distance_feature,
                 kremlin_coords=KREMLIN_COORDS).pipe(
                     add_walk_time_to_metro_feature,
-                    average_walk_speed=AVERAGE_WALK_SPEED).pipe(process_df).pipe(reduce_mem_usage))
+                    average_walk_speed=AVERAGE_WALK_SPEED).pipe(process_df).drop(
+                        FEATURE2DROP,
+            axis=1).pipe(reduce_mem_usage))
     return df
